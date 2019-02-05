355b55acb9d19f7b48a030ef9ac2427992235021
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-05 22:51:59 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,7 @@ def bonus
 
   #code your solution here:
 change= epic_tragedy[:montague[:hero[:status]]]
-change[2]="dead"
+change[0]="dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
